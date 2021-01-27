8356656d3cd8e076108715d6445abdae5b6879e8
Gabriel Gutiérrez Santillan <gabriel.gutierrez@infosys.com>
2021-01-27 11:39:27 -0600

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+class Home extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello World</h1>
+                <h1>From GitHub</h1>
+            </div>
+        )
+    }
+}
+export default Home;
